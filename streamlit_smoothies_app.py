4894d539729fd107e98aa722abf3891625251983
agitator23 <164179172+agitator23@users.noreply.github.com>
2024-03-20 06:34:21 -0700

    Update streamlit_smoothies_app.py
    moved fruityvice_response code inside for fruit_chosen block
    
    moved requests import to top (with other imports)

--- streamlit_smoothies_app.py
+++ streamlit_smoothies_app.py
@@ -1,7 +1,7 @@
 # Import python packages
 import streamlit as st
 #from snowflake.snowpark.context import get_active_session
-from snowflake.snowpark.functions import col
+from snowflake.snowpark.functions import col, requests
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
@@ -31,6 +31,8 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fv_df = st.dataframe(data = fruityvice_response.json(), use_container_width = True)
         
     st.write('string is', ingredients_string)
 
@@ -45,8 +47,3 @@ if ingredients_list:
 #        st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="âœ…")
         st.success('Your Smoothie is ordered, ' + name_on_order + '!')
 
-# add fruityvice additional data
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#st.text(fruityvice_response.json())
-fv_df = st.dataframe(data = fruityvice_response.json(), use_container_width = True)

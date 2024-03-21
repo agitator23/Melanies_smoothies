693daf7162d5f7d8fa0ac8cedb3e7d6ba2c640d0
agitator23 <164179172+agitator23@users.noreply.github.com>
2024-03-21 00:40:05 -0700

    Update streamlit_smoothies_app.py
    added Pandas library and new pandas dataframe

--- streamlit_smoothies_app.py
+++ streamlit_smoothies_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+import Pandas as pd
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 import requests
@@ -16,7 +17,11 @@ st.write('The name on your smoothie will be', name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('search_on')).sort(col('search_on'))
-st.dataframe(data=my_dataframe, use_container_width=True)
+#st.dataframe(data=my_dataframe, use_container_width=True)
+
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients'
